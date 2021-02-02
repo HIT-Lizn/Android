55dd20478bfdfaa01aea9a09445c060bacef8060
lizn <lixin8@oppo.com>
2021-02-02 23:09:52 +0800

    mediaplayer and sufaceview

--- JNITest/app/src/main/java/com/example/jnitest/MainActivity.java
+++ JNITest/app/src/main/java/com/example/jnitest/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.jnitest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+
+    private MediaPlayer mediaPlayer;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Context appcontext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +38,49 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        // mediaplayer example
+        try {
+            appcontext = this.getApplicationContext();
+            surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+            surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                    mediaPlayer = MediaPlayer.create(appcontext, R.raw.lizn);
+                    mediaPlayer.setDisplay(surfaceHolder);
+                    surfaceHolder.setKeepScreenOn(true);
+                    surfaceHolder.setFixedSize(1200, 900);
+                }
+
+
+
+                @Override
+                public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                }
+                else {
+                    mediaPlayer.start();
+                }
+            }
+        });
     }
 
     /**
